0ed00ea85b1bed0664c1a8ea4746291b89ecef1a
Michael Reichenbach <michael.reichenbach@interhyp.de>
2021-02-02 11:52:32 +0100

    feat: improve backup script with small cli

--- backup.sh
+++ backup.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-./dc.sh exec backup rcb backup --no-cleanup
+while [ -n "$1" ]; do
+    case "$1" in
+        now) echo "starting backup..."
+            ./dc.sh exec backup rcb backup --no-cleanup
+            ;;
+        cleanup) echo "cleaning up old backups..."
+            ./dc.sh exec backup rcb prune
+            ;;
+        snapshots)
+            ./dc.sh exec backup rcb snapshots
+            ;;
+        attach)
+            ./dc.sh exec -it backup sh
+            ;;
+        --) shift
+            break ;;
+        *) echo "$1 is not an option. Valid options: now, cleanup, snapshots or attach";;
+    esac
+    shift
+done
